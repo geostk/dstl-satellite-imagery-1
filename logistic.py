d2711745e8736ae19159a040c42302fe150d02b0
Michael Toth <michael@orchardplatform.com>
2017-01-25 16:40:46 -0500

    Adding patches & standard scalers

--- logistic.py
+++ logistic.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot
 import pandas as pd
 from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import average_precision_score
@@ -19,13 +20,13 @@ csv.field_size_limit(sys.maxsize);
 
 
 def get_scalers(image_size, x_max, y_min):
-    h, w = image_size  # they are flipped so that mask_for_polygons works correctly
+    h, w = image_size  # they are flipped so that get_polygon_mask works correctly
     w_ = w * (w / (w + 1))
     h_ = h * (h / (h + 1))
     return w_ / x_max, h_ / y_min
 
 
-def mask_for_polygons(polygons, image_size):
+def get_polygon_mask(polygons, image_size):
     img_mask = np.zeros(image_size, np.uint8)
     if not polygons:
         return img_mask
@@ -38,19 +39,7 @@ def mask_for_polygons(polygons, image_size):
     return img_mask
 
 
-def scale_percentile(matrix):
-    w, h, d = matrix.shape
-    matrix = np.reshape(matrix, [w * h, d]).astype(np.float64)
-    # Get 2nd and 98th percentile
-    mins = np.percentile(matrix, 1, axis=0)
-    maxs = np.percentile(matrix, 99, axis=0) - mins
-    matrix = (matrix - mins[None, :]) / maxs[None, :]
-    matrix = np.reshape(matrix, [w, h, d])
-    matrix = matrix.clip(0, 1)
-    return matrix
-
-
-def mask_to_polygons(mask, epsilon=10., min_area=10.):
+def convert_mask_to_polygons(mask, epsilon=10., min_area=10.):
     # first, find contours with cv2: it's much faster than shapely
     image, contours, hierarchy = cv2.findContours(
         ((mask == 1) * 255).astype(np.uint8),
@@ -90,139 +79,132 @@ def mask_to_polygons(mask, epsilon=10., min_area=10.):
     return all_polygons
 
 
-def train(train_ids):
+def train(train_ids, class_id, polys, gs, patch_size):
+    print('TRAINING...')
 
+    # First calculate standard scaler parameters
+    scaler = StandardScaler()
     for img_id in train_ids:
-        print('Operating on ' + str(img_id))
+        im_rgb = tiff.imread('input/three_band/{}.tif'.format(img_id)).transpose([1, 2, 0])
+        patches = extract_patches_2d(im_rgb, patch_size)
+        patches = np.reshape(patches, (len(patches), -1))
+        #xs = im_rgb.reshape(-1, 3).astype(np.float32)
+        xs = patches.astype(np.float32)
+        scaler.partial_fit(xs)
 
-        cur_polygons = df[(df['ImageId'] == img_id) & (df['ClassType'] == POLY_TYPE)].iloc[0]['MultipolygonWKT']
-        train_polygons = shapely.wkt.loads(cur_polygons)
+    # Next build the logistic model
+    model = SGDClassifier(loss='log')
+    for img_id in train_ids:
+        print('Training on ' + str(img_id) + ' for class ' + str(class_id))
 
-        # Load grid size
+        # Load grid size for current image polygon coordinates
         x_max, y_min = gs[gs['ImageId'] == img_id].iloc[0,1:].astype(float)
 
-        # Read image with tiff
+        # Read current image with tiff
         im_rgb = tiff.imread('input/three_band/{}.tif'.format(img_id)).transpose([1, 2, 0])
         im_size = im_rgb.shape[:2]
+        patches = extract_patches_2d(im_rgb, patch_size)
+        print(len(patches))
+        patches = np.reshape(patches, (len(patches), -1))
 
+
+        # Read in polygons for current image
+        cur_polygons = polys[(polys['ImageId'] == img_id) & (polys['ClassType'] == class_id)].iloc[0]['MultipolygonWKT']
+        train_polygons = shapely.wkt.loads(cur_polygons)
+
+        
         x_scaler, y_scaler = get_scalers(im_size, x_max, y_min)
         train_polygons_scaled = shapely.affinity.scale(train_polygons, xfact=x_scaler, yfact=y_scaler, origin=(0, 0, 0))
-        train_mask = mask_for_polygons(train_polygons_scaled, im_size)
+        train_mask = get_polygon_mask(train_polygons_scaled, im_size)
 
-        xs = im_rgb.reshape(-1, 3).astype(np.float32)
-        ys = train_mask.reshape(-1)
+        # Load xs from image and ys from polygon mask
+        #xs = im_rgb.reshape(-1, 3).astype(np.float32)
+        xs = patches.astype(np.float32)
+        ys = train_mask[0:-1, 0:-1].reshape(-1) # Drop last row and column to account for grid size
+        #ys = train_mask.reshape(-1)
 
-        #pipeline = make_pipeline(StandardScaler(), SGDClassifier(loss='log'))
+        # Scale x values with trained scaler
+        #print(xs.mean(axis=0))
+        xs = scaler.transform(xs)
+        print(im_rgb.shape)
+        print(xs.shape)
+        print(ys.shape)
+        #print(xs.mean(axis=0))
 
         print('training partial fit...')
-        # do not care about overfitting here
-        model = SGDClassifier(loss='log')
         model.partial_fit(xs, ys, classes = (0, 1))
 
-    return(model)
+    return scaler, model
 
 
-def predict(test_ids, model):
+def predict(test_ids, class_id, gs, scaler, model, patch_size):
 
     d = []
 
     for img_id in test_ids:
-        print('Operating on ' + str(img_id))
-        # Load grid size
+        print('Predicting on ' + str(img_id) + ' for class ' + str(class_id))
+        
+        # Load grid size for current image polygon coordinates
         x_max, y_min = gs[gs['ImageId'] == img_id].iloc[0,1:].astype(float)
 
-        # Read image with tiff
+        # Read current image with tiff
         im_rgb = tiff.imread('input/three_band/{}.tif'.format(img_id)).transpose([1, 2, 0])
         im_size = im_rgb.shape[:2]
+        patches = extract_patches_2d(im_rgb, patch_size)
+        patches = np.reshape(patches, (len(patches), -1))
 
-        x_scaler, y_scaler = get_scalers(im_size, x_max, y_min)
-        xs = im_rgb.reshape(-1, 3).astype(np.float32)
+        # Scale x values with trained scaler
+        #xs = im_rgb.reshape(-1, 3).astype(np.float32)
+        xs = patches.astype(np.float32)
+        xs = scaler.transform(xs)
         
+        # Predict pixel-level probabilities and apply 0.3 threshold for binary pixel mask
         pred_ys = model.predict_proba(xs)[:, 1]
-        pred_mask = pred_ys.reshape(im_size) # 3348 3403
+        #pred_mask = pred_ys.reshape(im_size) # 3348 3403
+        
+        temp_mask = pred_ys.reshape(np.subtract(im_size, 1)) # To deal with patch indexing
+        pred_mask = np.zeros((temp_mask.shape[0]+1, temp_mask.shape[1]+1)) # Pad with additional row and column of zeros
+        pred_mask[:-1,:-1] = temp_mask # Pad with additional row and column of zeros
+
         threshold = 0.3
         pred_binary_mask = pred_mask >= threshold
-        pred_polygons = mask_to_polygons(pred_binary_mask)
-        pred_poly_mask = mask_for_polygons(pred_polygons, im_size)
 
-        scaled_pred_polygons = shapely.affinity.scale(
-        pred_polygons, xfact=1 / x_scaler, yfact=1 / y_scaler, origin=(0, 0, 0))
+        # Convert pixel-level mask to polygon mask
+        pred_polygons = convert_mask_to_polygons(pred_binary_mask)
 
+        # Scale polygon values from image coordinates to grid size coordinates
+        x_scaler, y_scaler = get_scalers(im_size, x_max, y_min)
+        scaled_pred_polygons = shapely.affinity.scale(pred_polygons, xfact=1 / x_scaler, yfact=1 / y_scaler, origin=(0, 0, 0))
+
+        # Convert polygons to WKT format
         dumped_prediction = shapely.wkt.dumps(scaled_pred_polygons)
-        print('Prediction size: {:,} bytes'.format(len(dumped_prediction)))
         final_polygons = shapely.wkt.loads(dumped_prediction)
-        d.append({'ImageId': img_id, 'ClassType': POLY_TYPE, 'MultipolygonWKT': final_polygons})
+        print({'ImageId': img_id, 'ClassType': class_id, 'MultipolygonWKT': final_polygons})
+        d.append({'ImageId': img_id, 'ClassType': class_id, 'MultipolygonWKT': final_polygons})
 
     return(pd.DataFrame(d))
 
 
-#img_id = '6120_2_2'
-POLY_TYPE = 1  # buildings
+class_ids = [1]
+patch_size = (2, 2)
+#class_ids = (1,2,3,4,5,6,7,8,9,10)
 
-# Load train poly with shapely
-train_polygons = None
-df = pd.read_csv('input/train_wkt_v4.csv')
-train_ids = df['ImageId'].unique()
+#train_polygons = None
+polys = pd.read_csv('input/train_wkt_v4.csv')
 
 # Load grid sizes
 gs = pd.read_csv('input/grid_sizes.csv', names=['ImageId', 'Xmax', 'Ymin'], skiprows=1)
+
+# Extract train and test IDs from data
+train_ids = polys['ImageId'].unique()
 test_ids = gs[~gs.isin(train_ids)].dropna()['ImageId']
 
-model = train(train_ids)
-predictions = predict(test_ids, model)
+# Train model by class & Generate Predictions
+predictions = pd.DataFrame()
+for class_id in class_ids:
+    scaler, model = train(train_ids, class_id, polys, gs, patch_size)
+    new_predictions = predict(test_ids, class_id, gs, scaler, model, patch_size)
+    predictions = pd.concat([predictions, new_predictions])
 
-predictions.to_csv('output/temp_logistic_buildings_all_train.csv', index = False)
+predictions.to_csv('output/temp_logistic_buildings_all_train_scaled_patches_5_5.csv', index = False)
 
-#submission = pd.read_csv('input/sample_submission.csv')
-
-#new_submission = submission
-#new_submission.update(predictions['MultipolygonWKT'], overwrite=True)
-#new_submission.to_csv('output/logistic_buildings.csv', index = False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Predictions on New Images
-# pred_ys = pipeline.predict_proba(xs)[:, 1]
-# print('average precision', average_precision_score(ys, pred_ys))
-# pred_mask = pred_ys.reshape(train_mask.shape)
-
-# threshold = 0.3
-# pred_binary_mask = pred_mask >= threshold
-
-# # check jaccard on the pixel level
-# tp, fp, fn = (( pred_binary_mask &  train_mask).sum(),
-#               ( pred_binary_mask & ~train_mask).sum(),
-#               (~pred_binary_mask &  train_mask).sum())
-# print('Pixel jaccard', tp / (tp + fp + fn))
-
-
-# pred_polygons = mask_to_polygons(pred_binary_mask)
-# pred_poly_mask = mask_for_polygons(pred_polygons, im_size)
-
-# scaled_pred_polygons = shapely.affinity.scale(
-#     pred_polygons, xfact=1 / x_scaler, yfact=1 / y_scaler, origin=(0, 0, 0))
-
-# dumped_prediction = shapely.wkt.dumps(scaled_pred_polygons)
-# print('Prediction size: {:,} bytes'.format(len(dumped_prediction)))
-# final_polygons = shapely.wkt.loads(dumped_prediction)
-
-# print('Final jaccard',
-#       final_polygons.intersection(train_polygons).area /
-#       final_polygons.union(train_polygons).area)
