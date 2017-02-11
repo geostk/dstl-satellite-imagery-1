48c14061a5ce7d32c02645df7212d1b42dcbe955
Michael Toth <michael@orchardplatform.com>
2017-02-11 14:11:38 -0500

    Script to generate whole-image masks

---
+++ generate_train_masks.py
@@ -0,0 +1,86 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import cv2
+from shapely.wkt import loads as wkt_loads
+import tifffile as tiff
+
+
+def _convert_coordinates_to_raster(coords, img_size, xymax):
+    Xmax,Ymax = xymax
+    H,W = img_size
+    W1 = 1.0*W*W/(W+1)
+    H1 = 1.0*H*H/(H+1)
+    xf = W1/Xmax
+    yf = H1/Ymax
+    coords[:,1] *= yf
+    coords[:,0] *= xf
+    coords_int = np.round(coords).astype(np.int32)
+    return coords_int
+
+
+def _get_xmax_ymin(grid_sizes_panda, imageId):
+    xmax, ymin = grid_sizes_panda[grid_sizes_panda.ImageId == imageId].iloc[0,1:].astype(float)
+    return (xmax,ymin)
+
+
+def _get_polygon_list(wkt_list_pandas, imageId, cType):
+    df_image = wkt_list_pandas[wkt_list_pandas.ImageId == imageId]
+    multipoly_def = df_image[df_image.ClassType == cType].MultipolygonWKT
+    polygonList = None
+    if len(multipoly_def) > 0:
+        assert len(multipoly_def) == 1
+        polygonList = wkt_loads(multipoly_def.values[0])
+    return polygonList
+
+
+def _get_and_convert_contours(polygonList, raster_img_size, xymax):
+    perim_list = []
+    interior_list = []
+    if polygonList is None:
+        return None
+    for k in range(len(polygonList)):
+        poly = polygonList[k]
+        perim = np.array(list(poly.exterior.coords))
+        perim_c = _convert_coordinates_to_raster(perim, raster_img_size, xymax)
+        perim_list.append(perim_c)
+        for pi in poly.interiors:
+            interior = np.array(list(pi.coords))
+            interior_c = _convert_coordinates_to_raster(interior, raster_img_size, xymax)
+            interior_list.append(interior_c)
+    return perim_list,interior_list
+
+
+def _plot_mask_from_contours(raster_img_size, contours, class_value = 1):
+    img_mask = np.zeros(raster_img_size,np.uint8)
+    if contours is None:
+        return img_mask
+    perim_list,interior_list = contours
+    cv2.fillPoly(img_mask,perim_list,class_value)
+    cv2.fillPoly(img_mask,interior_list,0)
+    return img_mask
+
+
+def generate_mask_for_image_and_class(raster_size, imageId, class_type, grid_sizes_panda,
+                                     wkt_list_pandas):
+    xymax = _get_xmax_ymin(grid_sizes_panda,imageId)
+    polygon_list = _get_polygon_list(wkt_list_pandas,imageId,class_type)
+    contours = _get_and_convert_contours(polygon_list,raster_size,xymax)
+    mask = _plot_mask_from_contours(raster_size,contours,1)
+    return mask
+
+
+inDir = 'input'
+maskDir = 'input/masks'
+
+# read the training data from train_wkt_v4.csv
+df = pd.read_csv(inDir + '/train_wkt_v4.csv')
+train_ids = df['ImageId'].unique()
+
+for img_id in train_ids:
+    print('Operating on ' + str(img_id))
+    for class_id in range(1, 11):
+        
+        gs = pd.read_csv(inDir + '/grid_sizes.csv', names=['ImageId', 'Xmax', 'Ymin'], skiprows=1)
+        
+        mask = generate_mask_for_image_and_class((256, 256), img_id, class_id, gs, df)
+        cv2.imwrite(maskDir + '/' + str(img_id) + '_' + str(class_id) + '.png', mask * 255)
