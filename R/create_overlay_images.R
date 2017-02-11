a138cfb7931d653937dbbe028edc6cb0aa98e5fb
Michael Toth <michael@orchardplatform.com>
2017-02-11 14:07:45 -0500

    Updating overlay image code

--- R/create_overlay_images.R
+++ R/create_overlay_images.R
@@ -13,6 +13,7 @@ setwd('~/dev/dstl-satellite-imagery/R')
 
 library(raster)      # to read in from .tiff
 library(rgdal)       # to read in from wkt
+library(rgeos)
 library(geojsonio)   # to read in from geojson
 
 library(data.table)  # fast csv read
@@ -26,39 +27,17 @@ wkt = fread('../input/train_wkt_v4.csv')
 # change 'MULTIPOLYGON EMPTY' to NA
 wkt[wkt$MultipolygonWKT=='MULTIPOLYGON EMPTY','MultipolygonWKT'] = NA
 
-
 # define a colour pallette for polygon overlays
 col0 = rainbow(n=10, alpha = .5)
 
-# dataframe to map the many polygon types to 10 classes
-filename_to_classType = as.data.frame(matrix(c(
-  '001_MM_L2_LARGE_BUILDING',1,
-  '001_MM_L3_RESIDENTIAL_BUILDING',1,
-  '001_MM_L3_NON_RESIDENTIAL_BUILDING',1,
-  '001_MM_L5_MISC_SMALL_STRUCTURE',2,
-  '002_TR_L3_GOOD_ROADS',3,
-  '002_TR_L4_POOR_DIRT_CART_TRACK',4,
-  '002_TR_L6_FOOTPATH_TRAIL',4,
-  '006_VEG_L2_WOODLAND',5,
-  '006_VEG_L3_HEDGEROWS',5,
-  '006_VEG_L5_GROUP_TREES',5,
-  '006_VEG_L5_STANDALONE_TREES',5,
-  '007_AGR_L2_CONTOUR_PLOUGHING_CROPLAND',6,
-  '007_AGR_L6_ROW_CROP',6, 
-  '008_WTR_L3_WATERWAY',7,
-  '008_WTR_L2_STANDING_WATER',8,
-  '003_VH_L4_LARGE_VEHICLE',9,
-  '003_VH_L5_SMALL_VEHICLE',10,
-  '003_VH_L6_MOTORBIKE',10), ncol=2, byrow=T))
-filename_to_classType$V1 = as.character(filename_to_classType$V1)
-filename_to_classType$V2 = as.numeric(as.character(filename_to_classType$V2))
+predictions <- read.csv('~/dev/dstl-satellite-imagery/output/temp_xgboost_buildings_all_train_scaled_patches_256_3_3.csv', stringsAsFactors = F)
 
 # read in the polygon grid sizes which will be needed to
 # transform polys to same coords as rasters
 grid_sizes = fread('../input/grid_sizes.csv')
 setnames(grid_sizes, 'V1','ImageId')
 
-img_id <- '6100_2_2'
+img_id <- '6120_2_2'
 for(img_id in unique(wkt$ImageId)) {
   
   print(img_id)
@@ -94,39 +73,38 @@ for(img_id in unique(wkt$ImageId)) {
   # now plot the raster image
   plotRGB(img_raster, stretch = "lin")
   
+  poly <- readWKT(predictions[predictions$ImageId==img_id & predictions$ClassType == 1,]$MultipolygonWKT)
+  #poly <- readWKT(wkt[wkt$ImageId==img_id & wkt$ClassType == 1,]$MultipolygonWKT)
+  
+  plotRGB(img_raster, stretch = "lin")
+  plot(applyTransformation(at, poly), add = TRUE, col=col0[1])
+  
   # then loop through the 10 other polygon classes
   # load and transform the polygons using affine trans
   # plot in overlay on raster
   
-  for(i in 1:10) {
-    
-    class = filename_to_classType[filename_to_classType[,2]==i, 1]
-    file00 = NULL
-    
-    for(cl in class) file00 = c(file00, dir(pattern=cl, path=paste0('../input/train_geojson_v3/', img_id), full.names = T))
-    
-    if(length(file00)!=0) {
-      for(file0 in file00) {
-        shp_poly <- readOGR(file0, 'OGRGeoJSON',require_geomType='wkbPolygon',  p4s="")
-        plot(applyTransformation(at, shp_poly), add= TRUE, col=col0[i])
-      }
-    }
-  }
-  dev.off()
+  # for(i in 1:10) {
+  #   
+  #   class = filename_to_classType[filename_to_classType[,2]==i, 1]
+  #   file00 = NULL
+  #   
+  #   for(cl in class) file00 = c(file00, dir(pattern=cl, path=paste0('../input/train_geojson_v3/', img_id), full.names = T))
+  #   
+  #   if(length(file00)!=0) {
+  #     for(file0 in file00) {
+  #       shp_poly <- readOGR(file0, 'OGRGeoJSON',require_geomType='wkbPolygon',  p4s="")
+  #       plot(applyTransformation(at, shp_poly), add= TRUE, col=col0[i])
+  #     }
+  #   }
+  # }
+  # dev.off()
 }
 
-png('legend.png')
-barplot(1:10,col=col0[1:10])
-legend('topleft', legend=c('1 building','2 small struct', '3 good roads',
-                           '4 track/trial','5 VEG','6 ARG','7 Waterway','8 water',
-                           '9 large vh','10 small vh'),pch=15,col=col0[1:10])
 
 
 
 
-
-
-img_id <- '6100_2_2'
+img_id <- '6120_2_2'
 img_raster <- stack(paste0("../input/sixteen_band/", img_id, "_M.tif"))
 img_raster <- stack(paste0("../input/three_band//", img_id, ".tif"))
 plot(img_raster[[1]])
